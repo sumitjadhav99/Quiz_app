9df9ad0aa470f9176d28bbdd6d9cf59b25731733
sumit <sumithjadhav99@gmail.com>
2023-07-06 18:50:39 +0530

    First Commit

---
+++ src/Components/QuizResult.js
@@ -0,0 +1,15 @@
+function QuizResult({ score, totalScore, tryAgain }) {
+  return (
+    <>
+      <div className='show-score'>
+        Your Score: {score}
+        <br />
+        Total Score: {totalScore}
+      </div>
+      <button id='next-button' onClick={tryAgain}>
+        Try Again
+      </button>
+    </>
+  )
+}
+export default QuizResult
